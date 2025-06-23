3f52b50c930e212e93d2d70eb64e4d13e002067d
Sumrian Shi <3092787775@qq.com>
2025-06-23 17:59:36 +0800

    feat: 生成器小技巧

---
+++ js/利用生成器编写可迭代对象.js
@@ -0,0 +1,12 @@
+class Foo {
+    constructor() {
+        this.values = [1, 2, 3];
+    }
+    *[Symbol.iterator]() {
+        yield* this.values;
+    }
+}
+const f = new Foo();
+for (const x of f) {
+    console.log(x);
+}
